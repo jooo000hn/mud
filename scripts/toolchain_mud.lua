a8bafda45c81c2f63b3c30212e1f4399a428db54
Hugo Amnov <hugo.amiard@laposte.net>
2018-06-02 22:10:57 +0200

    Clang module fix

--- scripts/toolchain_mud.lua
+++ scripts/toolchain_mud.lua
@@ -112,7 +112,7 @@ function mud_module(name, root_path, subpath, preproc_name)
             
     configuration { "cpp-modules", "*-clang*" }
         buildoptions {
-            "-fmodule-file=" .. path.join(module_path, "Module.pcm")
+            "-fmodule-file=($OBJDIR)" .. path.join(subpath, "Module.pcm")
         }
         
         files {
